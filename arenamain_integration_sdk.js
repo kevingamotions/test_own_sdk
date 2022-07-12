4e45ed561be75094dc90e404a6871de54eea5e8e
Kevin Haryo Kuncoro <kevin.haryo@gamotions.com>
2022-07-12 10:04:05 +0700

    initial commit

---
+++ arenamain_integration_sdk.js
@@ -0,0 +1,92 @@
+/*
+  Kevin Haryo Kuncoro
+  kevin.haryo@gamotions.com
+  kevinhyokun91@gmail.com
+*/
+
+var style = document.createElement('style');
+style.type = 'text/css';
+style.innerHTML = '.arenamain_touchpoint { cursor: pointer; }';
+document.getElementsByTagName('head')[0].appendChild(style);
+
+// var host_url = window.location.href;
+
+// height in percent
+var navbar_height = 5;
+var iframe_height = 100 - navbar_height;
+
+var arenamain_anchor = document.getElementById("arenamain_anchor");
+arenamain_anchor.style.display = "none";
+arenamain_anchor.style.width = "512px";
+arenamain_anchor.style.height = "512px";
+arenamain_anchor.style.backgroundColor = "gray";
+arenamain_anchor.style.position = "absolute";
+arenamain_anchor.style.top = "50%";
+arenamain_anchor.style.left = "50%";
+arenamain_anchor.style.transform = "translate(-50%, -50%)"
+
+var arenamain_anchor_custom_navbar = document.createElement('div');
+arenamain_anchor_custom_navbar.style.display = "flex";
+arenamain_anchor_custom_navbar.style.width = "100%";
+arenamain_anchor_custom_navbar.style.height = navbar_height.toString() + "%";
+arenamain_anchor_custom_navbar.style.backgroundColor = "black";
+arenamain_anchor_custom_navbar.style.justifyContent = "space-between";
+
+var custom_navbar_button_width = "auto";
+var custom_navbar_button_height = "80%";
+var custom_navbar_button_padding = "2px";
+var custom_navbar_button_cursor = "pointer";
+
+var arenamain_anchor_custom_navbar_back = document.createElement('img');
+arenamain_anchor_custom_navbar_back.style.height = custom_navbar_button_height;
+arenamain_anchor_custom_navbar_back.style.width = custom_navbar_button_width;
+arenamain_anchor_custom_navbar_back.style.padding = custom_navbar_button_padding;
+arenamain_anchor_custom_navbar_back.style.cursor = custom_navbar_button_cursor;
+arenamain_anchor_custom_navbar_back.src = "https://arenamain.id/portal/img/ss_left_btn.png";
+arenamain_anchor_custom_navbar_back.onclick = function(){nav_back();};
+
+var arenamain_anchor_custom_navbar_close = document.createElement('img');
+arenamain_anchor_custom_navbar_close.style.height = custom_navbar_button_height;
+arenamain_anchor_custom_navbar_close.style.width = custom_navbar_button_width;
+arenamain_anchor_custom_navbar_close.style.padding = custom_navbar_button_padding;
+arenamain_anchor_custom_navbar_close.style.cursor = custom_navbar_button_cursor;
+arenamain_anchor_custom_navbar_close.src = "https://arenamain.id/portal/img/ss_red_close_btn.png";
+arenamain_anchor_custom_navbar_close.onclick = function(){document.exitFullscreen();};
+
+arenamain_anchor_custom_navbar.appendChild(arenamain_anchor_custom_navbar_back);
+arenamain_anchor_custom_navbar.appendChild(arenamain_anchor_custom_navbar_close);
+
+var arenamain_iframe = document.getElementById("arenamain_iframe");
+arenamain_iframe.style.width = "100%";
+arenamain_iframe.style.height = iframe_height.toString() + "%";
+arenamain_iframe.frameBorder = "0";
+arenamain_iframe.allow = " ";
+
+arenamain_anchor.insertBefore(arenamain_anchor_custom_navbar, arenamain_iframe);
+
+function show_arenamain_anchor(){
+    if(arenamain_anchor.style.display === "none"){
+
+	arenamain_anchor.style.display = "block";
+
+	if(arenamain_anchor.requestFullscreen){
+	    arenamain_anchor.requestFullscreen();
+	}
+	else if(arenamain_anchor.webkitRequestFullscreen){
+	    arenamain_anchor.webkitRequestFullscreen();
+	}
+
+    }
+}
+
+function hide_arenamain_anchor(){
+    if(!document.fullscreenElement) {
+	arenamain_anchor.style.display = "none";
+    }
+}
+
+function nav_back(){
+    history.back();
+}
+
+document.documentElement.addEventListener('fullscreenchange', hide_arenamain_anchor);
